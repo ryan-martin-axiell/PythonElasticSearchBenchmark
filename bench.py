ea3ece387865a23412c35b54db6c6bd73f1ada91
axiell <axiell@piccadilly.man.kesoftware.com>
2019-01-17 23:16:13 +0000

    Inital commit

---
+++ bench.py
@@ -0,0 +1,50 @@
+from elasticsearch import Elasticsearch
+import uuid
+import time
+import sys
+
+if (len(sys.argv) < 2):
+	print("missing arguments")
+	sys.exit(1)
+
+table = sys.argv[1]
+
+fileuuid = table + "." + str(uuid.uuid4())
+fileuuid = "/dev/null"
+
+es = Elasticsearch()
+
+base = {
+	"_source": "id",
+	"sort": [
+		{ "id.keyword": "asc" }
+	],
+	"size": 5000
+}
+
+start = time.time()
+
+with open(fileuuid, 'w') as f:
+	res = es.search(index=table, doc_type="_doc", body=base)
+	count = len(res['hits']['hits'])
+	size = res['hits']['total']
+	last = res['hits']['hits'][-1]
+
+	f.write(str(res['hits']['hits']))
+
+	while(count <= size):
+		base['search_after'] = last['sort']
+		res = es.search(index=table, doc_type="_doc", body=base)
+		hits = res['hits']['hits']
+
+		f.write(str(hits))
+
+		count += len(hits)
+		if (len(hits) == 0):
+			break
+
+		last = hits[-1]
+		print("{}/{} last id: {}".format(count,size,last['_source']['id']))
+
+end = time.time()
+print(str(end - start) + "s")
